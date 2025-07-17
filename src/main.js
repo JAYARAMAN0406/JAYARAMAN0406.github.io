3f702d2022cc0bddbdb92506f9f6b345482d7924
Jayaraman040699 <mjrjpr0499@gmail.com>
2025-07-17 17:07:37 +0530

    uy

--- src/main.js
+++ src/main.js
@@ -4,10 +4,10 @@ import App from './App.js';
 import './index.css';
 
 const container = document.getElementById('root');
+
 if (container) {
   createRoot(container).render(
-    React.createElement(React.StrictMode, null, 
-      React.createElement(App)
-    )
-  );
+
+      <App />
+  )
 }
